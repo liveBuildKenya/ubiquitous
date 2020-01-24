8b9a81c0b01dc6b14447e1ebb264d0a1b7583edd
Ezra Mungai <ezra.mungai@sibasi.ltd>
2020-01-24 12:45:02 +0300

    Implements the repository pattern

---
+++ AppDomain/Infrastructure/EfRepository.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Linq;
+using AppSeed;
+using AppSeed.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppDomain.Infrastructure
+{
+    public partial class EfRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        #region Fields
+
+        private readonly IDbContext _context;
+
+        private DbSet<TEntity> _entities;
+
+        #endregion
+
+        #region Ctor
+
+        public EfRepository(IDbContext context)
+        {
+            _context = context;
+        }
+        
+        #endregion
+
+        #region Utilities
+
+        /// <summary>
+        /// Rollback of entity changes and return full error message
+        /// </summary>
+        /// <param name="exception">Exception</param>
+        /// <returns>Error message</returns>
+        protected string GetFullErrorTextAndRollbackEntityChanges(DbUpdateException exception)
+        {
+            //rollback entity changes
+            if (_context is DbContext dbContext)
+            {
+                var entries = dbContext.ChangeTracker.Entries()
+                    .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified).ToList();
+
+                entries.ForEach(entry =>
+                {
+                    try
+                    {
+                        entry.State = EntityState.Unchanged;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // ignored
+                    }
+                });
+            }
+            
+            try
+            {
+                _context.SaveChanges();
+                return exception.ToString();
+            }
+            catch (Exception ex)
+            {
+                //if after the rollback of changes the context is still not saving,
+                //return the full text of the exception that occurred when saving
+                return ex.ToString(); 
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        public IQueryable<TEntity> Table => Entities;
+
+        protected virtual DbSet<TEntity> Entities
+        {
+            get
+            {
+                if (_entities == null)
+                    _entities = _context.Set<TEntity>();
+
+                    return _entities;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Deletes an entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual void Delete(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+            
+            try
+            {
+                Entities.Remove(entity);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException exception)
+            {
+                throw new Exception(GetFullErrorTextAndRollbackEntityChanges(exception), exception);
+            }
+        }
+
+        /// <summary>
+        /// Gets entity by idnetifier
+        /// </summary>
+        /// <param name="id">Identifier</param>
+        /// <returns>Entity</returns>
+        public virtual TEntity GetById(object id)
+        {
+            return Entities.Find(id);
+        }
+
+        /// <summary>
+        /// Insert entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual void Insert(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            try
+            {
+                Entities.Add(entity);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException exception)
+            {
+                //ensure that the detailed error text is saved in the Log
+                throw new Exception(GetFullErrorTextAndRollbackEntityChanges(exception), exception);
+            }
+        }
+
+        /// <summary>
+        /// Update entity
+        /// </summary>
+        /// <param name="entity">Entity</param>
+        public virtual void Update(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            try
+            {
+                Entities.Update(entity);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException exception)
+            {
+                //ensure that the detailed error text is saved in the Log
+                throw new Exception(GetFullErrorTextAndRollbackEntityChanges(exception), exception);
+            }
+        }
+
+        #endregion
+    }
+}
