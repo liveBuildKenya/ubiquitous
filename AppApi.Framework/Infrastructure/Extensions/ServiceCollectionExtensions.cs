ae4d4697e8bd3bc9f1ffa12306681b0fec3ec294
Ezra Mungai <ezra.mungai@sibasi.ltd>
2020-01-26 16:47:24 +0300

    Updates definition

--- AppApi.Framework/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ AppApi.Framework/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using AppServices.Infrastructure.Extensions;
 
@@ -9,7 +8,7 @@ namespace AppApi.Framework.Infrastructure.Extensions
     /// </summary>
     public static class ServiceCollectionExtensions
     {
-        public static void ConfigureApplicationServices(this IServiceCollection services, IConfigurationRoot configuration)
+        public static void ConfigureApplicationServices(this IServiceCollection services)
         {
             services.ConfigureAlbumModuleServices();
         }
