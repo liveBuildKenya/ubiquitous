3eb13cfa798fdbd673e62c56b91684cc73750cd0
Ezra Mungai <ezra.mungai@sibasi.ltd>
2020-01-26 20:11:21 +0300

    Adds tests that pass

--- AlbumService.Tests/AlbumServiceTest.cs
+++ AlbumService.Tests/AlbumServiceTest.cs
@@ -1,14 +1,34 @@
 using System;
+using AppDomain.Models;
+using AppSeed.Infrastructure.Data;
+using AppServices.Service;
+using Moq;
 using Xunit;
 
-namespace AlbumService.Tests
+namespace Service.Tests
 {
     public class AlbumServiceTest
     {
         [Fact]
         public void AlbumService_Given_Album_Id_Should_Get_Album()
         {
-            var album = new Album()
+            var album = new Album() {
+                Id = 1,
+                Name = "Name"
+            };
+
+            var albumRepositoryMock = new Mock<IRepository<Album>>();
+            var songReposistoryMock = new Mock<IRepository<Song>>();
+
+            albumRepositoryMock.Setup(m => m.GetById(1)).Returns(album).Verifiable();
+
+            var albumService = new AlbumService(albumRepositoryMock.Object, songReposistoryMock.Object);
+
+            var actual = albumService.GetAlbumById(1);
+
+            albumRepositoryMock.Verify();
+            Assert.NotNull(actual);
+            Assert.Equal("Name", actual.Name);
         }
     }
 }
