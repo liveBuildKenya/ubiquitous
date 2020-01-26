5096b7a5f8e38d65253d61f7b1b640f2101d9253
Ezra Mungai <ezra.mungai@sibasi.ltd>
2020-01-26 19:57:15 +0300

    Adds unit test

---
+++ AlbumService.Tests/AlbumServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AlbumService.Tests
+{
+    public class AlbumServiceTest
+    {
+        [Fact]
+        public void AlbumService_Given_Album_Id_Should_Get_Album()
+        {
+            var album = new Album()
+        }
+    }
+}
